2dfa3a124412e36ebddf4fc84017dc65e9cbc3d6
Tigran Grigoryan <tigrangrigoryan@tigrans-air.guest.visitor>
2019-07-05 17:15:48 +0100

    First commit

---
+++ src/components/cv.js
@@ -0,0 +1,12 @@
+import React, { component } from "react";
+
+class Cv extends component {
+  render() {
+    return (
+      <div>
+        <h1>CV Page</h1>
+      </div>
+    );
+  }
+}
+export default Cv;
